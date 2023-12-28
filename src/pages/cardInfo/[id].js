40018d709a75c25d55df55e1770ebc51109f978b
Azhigov Usman <109419417+UsmanAzhigov@users.noreply.github.com>
2023-12-28 20:22:51 +0300

    Update [id].js поправил
    Применил key prop к сопоставленным элементам.
    Скорректировал функции добавления в корзину и оплаты товара, чтобы они напрямую передавали товар, а не распространяли его на объект.
    По возможности удалил ненужные пробелы и встроенные стили.
    Использовал React.Фрагмент вместо возврата массива элементов.
    Применил имена классов для стилизации вместо встроенных стилей, где это применимо.
    Добавил отсутствующую зависимость в массив зависимостей use Effect ([router.query.id]).

--- src/pages/cardInfo/[id].js
+++ src/pages/cardInfo/[id].js
@@ -1,88 +1,85 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './cardInfo.module.scss';
 import { Image, Title } from '@mantine/core';
 import { Rate } from 'antd';
 import { useRouter } from 'next/router';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCardInfo } from '@/redux/cardInfo/slice';
-import { fetchAddItems, fetchPayProduct } from '@/redux/cart/slice'; // выровни и раздели
+import { fetchAddItems, fetchPayProduct } from '@/redux/cart/slice';
 
 const CardInfo = () => {
   const { items } = useSelector((state) => state.cardInfo);
   const dispatch = useDispatch();
   const router = useRouter();
 
-  React.useEffect(() => {
+  useEffect(() => {
     const id = router.query.id;
     dispatch(fetchCardInfo({ id }));
-  }, []); // отступы 
-  const addToCart = (obj) => {
+  }, [router.query.id]); // Removed unnecessary spaces
+
+  const addToCart = (item) => {
     try {
-      dispatch(fetchAddItems({ obj }));
+      dispatch(fetchAddItems({ item }));
       alert('Товар успешно добавлен в корзину:)');
       router.push('/');
     } catch (error) {
-      console.log(error);
+      console.error(error);
       alert('Не удалось добавить товар в корзину:(');
     }
   };
 
-  const payProduct = async (items) => {
+  const payProduct = async (item) => {
     try {
-      dispatch(fetchPayProduct(items));
-      alert('Товары успешно оформел и добавлен в профиль:)');
+      await dispatch(fetchPayProduct([item]));
+      alert('Товар успешно оформлен и добавлен в профиль:)');
       router.push('/');
     } catch (error) {
-      alert('Не удалось добавить товары в профиль');
+      console.error(error);
+      alert('Не удалось добавить товар в профиль');
     }
   };
 
   return (
     <div className={styles.cardInfo}>
-      {items.map((item) => {
-        return (
-          <>
-            <Title order={2}>{item.title}</Title>
-            <div className={styles.cardBlock}>
-              <div className={styles.productImage}>
-                <Image width={328} height={329} src={item.imageUrl} />
+      {items.map((item) => (
+        <React.Fragment key={item.id}>
+          <Title order={2}>{item.title}</Title>
+          <div className={styles.cardBlock}>
+            <div className={styles.productImage}>
+              <Image width={328} height={329} src={item.imageUrl} />
+            </div>
+            <div className={styles.productPrice}>
+              <div className={styles.price}>
+                <Title style={{ fontSize: 44 }}>{item.price} ₽</Title>
+                <Title className={styles.noDiscount} style={{ color: 'grey' }} order={2}>
+                  {item.discount} ₽
+                </Title>
               </div>
-              <div className={styles.productPrice}>
-                <div className={styles.price}>
-                  <Title style={{ fontSize: 44 }}>{item.price} ₽</Title>
-                  <Title className={styles.noDiscount} style={{ color: 'grey' }} order={2}>
-                    {item.discount} ₽
-                  </Title>
-                </div>
-                <div className={styles.addToCart}>
-                  <button className={styles.addBtn} onClick={() => addToCart({ ...item })}>
-                    Добавить в корзину
-                  </button>
-                  <button className={styles.payNow} onClick={() => payProduct(item)}>
-                    Купить сейчас
-                  </button>
-                </div>
-                <div className={styles.reviews}>
-                  <Title style={{ color: 'grey', fontWeight: 300 }} order={3}> // инлайн стили не используй, находи класс этого компонента и так
-                    {item.reviews}
-                  </Title>
-                  <p style={{ color: 'grey' }}>•</p>  ///инлайн стили
-                  <Rate
-                    style={{ color: 'rgba(255, 170, 0, 1)', fontSize: 15 }} // инлайн стили
-                    defaultValue={item.rating}
-                  />
-                </div>
+              <div className={styles.addToCart}>
+                <button className={styles.addBtn} onClick={() => addToCart(item)}>
+                  Добавить в корзину
+                </button>
+                <button className={styles.payNow} onClick={() => payProduct(item)}>
+                  Купить сейчас
+                </button>
+              </div>
+              <div className={styles.reviews}>
+                <Title className={styles.greyText} order={3}>
+                  {item.reviews}
+                </Title>
+                <p className={styles.greyText}>•</p>
+                <Rate className={styles.rating} defaultValue={item.rating} />
               </div>
             </div>
-            <div className={styles.describe}>
-              <Title order={3}>Цвет: {item.color}</Title>
-              <Title order={3}>Питание</Title>
-              <Title order={3}>Время зарядки _____________ {item.timeCharging}</Title> /// это можно сделать по другому (черточка)
-              <Title order={3}>Время работы от аккумулятора _____________ {item.lifeBattery}</Title> 
-            </div>
-          </>
-        );
-      })}
+          </div>
+          <div className={styles.describe}>
+            <Title order={3}>Цвет: {item.color}</Title>
+            <Title order={3}>Питание</Title>
+            <Title order={3}>Время зарядки: {item.timeCharging}</Title>
+            <Title order={3}>Время работы от аккумулятора: {item.lifeBattery}</Title>
+          </div>
+        </React.Fragment>
+      ))}
     </div>
   );
 };
